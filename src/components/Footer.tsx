ebbe5cead42a205dc9276ba327a09475da3f65e6
Yves IRAKOZE MFURA <132387277+yvesmfura@users.noreply.github.com>
2024-09-29 11:22:36 +0200

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,22 +4,22 @@ import { FaLinkedin, FaInstagram, FaGithub, FaEnvelope } from 'react-icons/fa';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <p>Developed by Yves IRAKOZE MFURA</p>
+      <p>Developed by Imfura Tech Solutions</p>
       <div className="social-links">
-        <a href="https://www.linkedin.com/in/yves-irakoze-mfura-203053252/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/company/imfuratechsolutions" target="_blank" rel="noopener noreferrer">
           <FaLinkedin size={24} />
         </a>
-        <a href="https://www.instagram.com/yves.mfura/" target="_blank" rel="noopener noreferrer">
+        <a href="https://instagram.com/imfuratechsolutions" target="_blank" rel="noopener noreferrer">
           <FaInstagram size={24} />
         </a>
-        <a href="https://github.com/yvesmfura" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/imfutatechsolutions" target="_blank" rel="noopener noreferrer">
           <FaGithub size={24} />
         </a>
-        <a href="mailto:mfuryves25@gmail.com">
+        <a href="mailto:info@imfuratechsolutions.com">
           <FaEnvelope size={24} />
         </a>
       </div>
-      <p>&copy; 2024 Yves IRAKOZE MFURA. All rights reserved.</p>
+      <p>&copy; 2024 Imfura Tech Solutions. All rights reserved.</p>
     </footer>
   );
 };
